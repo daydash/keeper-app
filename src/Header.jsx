1605df3fe5ec9f7fa24b89bafe5fd54218142c0a
daydash <thedaydash@gmail.com>
2023-03-28 04:26:40 +0530

    adding color options

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+// import HighlightIcon from "@material-ui/icons/Highlight";
 import HighlightIcon from "@mui/icons-material/Highlight";
+import InvertColorsIcon from "@mui/icons-material/InvertColors";
+import ColorLensIcon from "@mui/icons-material/ColorLens";
 
 function Header() {
 	return (
@@ -7,6 +10,17 @@ function Header() {
 			<h1>
 				<HighlightIcon />
 				Keeper
+			</h1>
+			<h1>
+				<div>
+					<button type="button">
+						<h1>
+							<ColorLensIcon fontSize="large" className="header-icons" />
+						</h1>
+					</button>
+					<input type="color" value="black"></input>
+					<InvertColorsIcon fontSize="large" className="header-icons" />
+				</div>
 			</h1>
 		</header>
 	);
