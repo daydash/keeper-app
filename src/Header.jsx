b633588a0b729267b53986d7c577cc66b525dc01
daydash <thedaydash@gmail.com>
2023-03-28 04:24:44 +0530

    breakpoints correction

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import HighlightIcon from "@mui/icons-material/Highlight";
-import InvertColorsIcon from "@mui/icons-material/InvertColors";
-import ColorLensIcon from "@mui/icons-material/ColorLens";
 
 function Header() {
 	return (
@@ -9,17 +7,6 @@ function Header() {
 			<h1>
 				<HighlightIcon />
 				Keeper
-			</h1>
-			<h1>
-				<div>
-					<button type="button">
-						<h1>
-							<ColorLensIcon fontSize="large" className="header-icons" />
-						</h1>
-					</button>
-					<input type="color" value="black"></input>
-					<InvertColorsIcon fontSize="large" className="header-icons" />
-				</div>
 			</h1>
 		</header>
 	);
